003fe301f31e559e40edf9c738faf6a903637428
Mike Dougherty <mdougherty@tagged.com>
2014-02-07 12:51:08 -0800

    Add some similar tests for Config.push

--- tests/tds/commands_test.py
+++ tests/tds/commands_test.py
@@ -5,33 +5,28 @@ import logging
 import tds.commands
 
 #class TestPromote(unittest2.TestCase):
-class TestPromote(unittest2.TestCase):
+class TestPromoteAndPush(unittest2.TestCase):
     def setUp(self):
 
-        self.deploy = tds.commands.Deploy(logging.getLogger(type(self).__name__))
+        self.deploy = tds.commands.Deploy(logging.getLogger(type(self).__name__ + ' Deploy'))
+        self.config = tds.commands.Config(logging.getLogger(type(self).__name__ + 'Config'))
 
         deploy_methods = [
-            'get_app_info',
-            'send_notifications',
-            'verify_project_type',
-            'perform_deployments',
-            'find_app_deployments',
-            'ensure_newer_versions',
-            'determine_new_deployments',
-            'ensure_explicit_destinations',
+            ('get_app_info', (None, None, None)),
+            ('send_notifications', None),
+            ('perform_deployments', None),
+            ('verify_project_type', 'fake_app'),
+            ('find_app_deployments', {}),
+            ('ensure_newer_versions', False),
+            ('determine_new_deployments', (None, None)),
+            ('ensure_explicit_destinations', None),
         ]
 
-        for key in deploy_methods:
-            ptch = patch.object(self.deploy, key)
-            setattr(self, key, ptch.start())
-
-        self.get_app_info.return_value = (None, None, None)
-        self.send_notifications.return_value = None
-        self.perform_deployments.return_value = None
-        self.verify_project_type.return_value = 'fake_app'
-        self.find_app_deployments.return_value = {}
-        self.determine_new_deployments.return_value = None, None
-        self.ensure_explicit_destinations.return_value = None
+        for (key, return_value) in deploy_methods:
+            for obj in [self.deploy, self.config]:
+                patcher = patch.object(obj, key)
+                ptch = patcher.start()
+                ptch.return_value = return_value
 
         self.session = patch('tds.commands.Session', **{'commit.return_value': None}).start()
         self.tds_authorize = patch('tds.authorize', **{'verify_access.return_value': True}).start()
@@ -40,7 +35,7 @@ class TestPromote(unittest2.TestCase):
         patch.stopall()
 
     def test_promote_new_version(self):
-        self.ensure_newer_versions.return_value = True
+        self.deploy.ensure_newer_versions.return_value = True
 
         self.deploy.promote(dict(
             user_level = 'fake_access',
@@ -51,7 +46,7 @@ class TestPromote(unittest2.TestCase):
         assert self.deploy.perform_deployments.called
 
     def test_promote_old_version(self):
-        self.ensure_newer_versions.return_value = False
+        self.deploy.ensure_newer_versions.return_value = False
 
         self.deploy.promote(dict(
             user_level = 'fake_access',
@@ -60,6 +55,29 @@ class TestPromote(unittest2.TestCase):
         ))
 
         assert not self.deploy.perform_deployments.called
+
+    def test_push_new_version(self):
+        self.config.ensure_newer_versions.return_value = True
+
+        self.config.push(dict(
+            user_level = 'fake_access',
+            environment = 'fake_env',
+            project = 'fake_app'
+        ))
+
+        assert self.config.perform_deployments.called
+
+    def test_push_old_version(self):
+        self.config.ensure_newer_versions.return_value = False
+
+        self.config.push(dict(
+            user_level = 'fake_access',
+            environment = 'fake_env',
+            project = 'fake_app'
+        ))
+
+        assert not self.config.perform_deployments.called
+
 
 
 if __name__ == '__main__':
