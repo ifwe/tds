72f1091406bdb528d217bc8be4bc7a50f606748e
Mike Dougherty <mdougherty@tagged.com>
2014-02-19 17:31:09 -0800

    Merge --squash branch 'origin/jenkins-dsl'
    Squashed commit of the following:
    
    commit d47f2a16020caca8ae2a1a655c806aaad8a565e6
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 17:17:55 2014 -0800
    
        Run pyflakes only for tds/ and bin/
    
    commit df133ed58f99d7b8b7202cacd16a0d6956d9319e
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 17:12:18 2014 -0800
    
        Coverage is wonky, I think this is why
    
    commit 8e66dd1605e70e77548c3a3b72c31b6750673cde
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 17:09:04 2014 -0800
    
        Ignore $VIRTUAL_ENV when linting/testing
    
    commit 74b2fc538327211f5a8e7bba5cea64b31ff806c3
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 17:03:56 2014 -0800
    
        First arg here is for console warnings, second is for files
    
    commit 18f4b05a667f8d552a13567237a4acd2b11a110f
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 17:00:52 2014 -0800
    
        Fix references to tagopsdb at v0.6.0-beta.2 tag
    
    commit 06f64d5fb03877a850e6e42136f15454489c3438
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 16:52:22 2014 -0800
    
        Apparently 'activate' is not executable
    
    commit fec6eacb9131915c6c7ebcac3424300bf039bdb3
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 16:46:31 2014 -0800
    
        Fix install for dev requirements
    
    commit 07fd24c282cf54c91c67d44b95d1ed058d5a2a01
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 16:45:12 2014 -0800
    
        Add virtualenv/bin to path
    
    commit 27d02490a6a2c23c4461561ffc351d37af90ce91
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 16:41:13 2014 -0800
    
        Apparently the 'label' option to Project does not get propagated. Also remove botched copy/paste ('security_project')
    
    commit d9d809ae67b2279e50d58d5ae962b2c06e32d21c
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 16:38:50 2014 -0800
    
        Fix order of operations for handling reports directory
    
        derp
    
    commit c713256fc69b82476932db3a71b83bf6b967f7df
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 16:36:58 2014 -0800
    
        Set executable bit on scripts
    
    commit 38fde50dcfa4413319b0df8aa5286735527c9c68
    Author: Mike Dougherty <mdougherty@tagged.com>
    Date:   Wed Feb 19 16:19:23 2014 -0800
    
        Initial attempt for jenkins dsl implementation

---
+++ .jenkins/jobs/suite.dsl.groovy
@@ -0,0 +1,45 @@
+@GrabResolver('https://artifactory.tagged.com/artifactory/libs-release-local/')
+@Grab('com.tagged.build:jenkins-dsl-common:0.1.11')
+
+import com.tagged.build.common.*
+
+def project = new Project(
+    jobFactory,
+    [
+        githubOwner: 'siteops',
+        githubProject: 'deploy',
+        hipchatRoom: 'Tagged Deployment System',
+        email: 'devtools@tagged.com',
+    ]
+)
+
+// Report pylint warnings and go 'unstable' when over the threshold
+def pylint = project.downstreamJob {
+    name 'pylint'
+    label 'python26 && centos6'
+    steps { shell '.jenkins/scripts/pylint.sh' }
+
+    publishers {
+        warnings([], ['Pyflakes': 'reports/pyflakes.log'])
+        violations {
+            pylint(10, 11, 10, "reports/pylint.log")
+        }
+    }
+}
+
+// Run python unit tests and record results
+def pyunit = project.downstreamJob {
+    name 'pyunit'
+    label 'python26 && centos6'
+    steps { shell '.jenkins/scripts/pyunit.sh' }
+    publishers {
+        archiveJunit "reports/pyunit.xml"
+        cobertura('coverage.xml')
+    }
+}
+
+def gauntlet = project.gauntlet([
+    ['Gauntlet', [pylint, pyunit]],
+])
+
+def (tds, branches) = project.branchBuilders(gauntlet.name)
