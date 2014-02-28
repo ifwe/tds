2c3b07592c5307a481815d4a980ebef542a8e1dc
Mike Dougherty <mdougherty@tagged.com>
2014-02-28 14:07:23 -0800

    Fixup pylint command

--- .jenkins/scripts/pylint.sh
+++ .jenkins/scripts/pylint.sh
@@ -4,7 +4,7 @@ scripts=$( dirname "${BASH_SOURCE-$0}" )
 source $scripts/python-setup.sh
 
 for d in tds bin ; do
-    pylint -f=parseable -r=n $d >> reports/pylint.log || :
+    pylint -f parseable -r=n $d >> reports/pylint.log || :
     flake8 --max-complexity=8 $d >> reports/pyflakes.log || :
 done
 
