ae2d263386ab4f79707dea954b76ce7dc8600948
Mike Dougherty <mdougherty@tagged.com>
2014-03-03 15:28:00 -0800

    Use -v (verbose) to aid debugging on jenkins

--- .jenkins/scripts/pyunit.sh
+++ .jenkins/scripts/pyunit.sh
@@ -4,6 +4,7 @@ scripts=$( dirname "${BASH_SOURCE-$0}" )
 source $scripts/python-setup.sh
 
 $WORKSPACE/run_tests.py \
+    -v \
     --junitxml=reports/pyunit.xml \
     --cov-report=xml \
     --cov=tds \
