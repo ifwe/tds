c014c9bccbb4a9af581ebf16ffc5aa8ce2abe461
Christopher A. Stelma <cstelma@tagged.com>
2014-03-06 09:59:50 -0800

    pyflakes indentation

--- tds/scripts/tds_prog.py
+++ tds/scripts/tds_prog.py
@@ -10,8 +10,8 @@ import tds.configure_logging as conflog
 import tds.version
 
 from tds.exceptions import AccessError, ConfigurationError, \
-                           WrongEnvironmentError, \
-                           WrongProjectTypeError
+    WrongEnvironmentError, \
+    WrongProjectTypeError
 
 
 def create_subparsers(parser):
@@ -46,9 +46,9 @@ def parse_command_line():
     parser.add_argument('-v', '--verbose', action='count',
                         help='Show more information (more used shows greater '
                              'information)')
-    parser.add_argument('--dbuser', help='Specify user to use to connect '
-                                         'to TagOpsDB',
-                                    default=None)
+    parser.add_argument('--dbuser',
+                        help='Specify user to use to connect to TagOpsDB',
+                        default=None)
     parser.add_argument('--disable-color', dest='use_color',
                         action='store_false',
                         help='Disable color coding when in verbose mode(s)')
