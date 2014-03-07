53d600be6fc8587ab913e15a01c085e3d72f78f3
Mike Dougherty <mdougherty@tagged.com>
2014-03-06 18:05:17 -0800

    No more bin directory, and add tests/ to linters

--- .jenkins/scripts/pylint.sh
+++ .jenkins/scripts/pylint.sh
@@ -3,8 +3,7 @@
 scripts=$( dirname "${BASH_SOURCE-$0}" )
 source $scripts/python-setup.sh
 
-for d in tds bin ; do
+for d in tds tests ; do
     pylint -f parseable -r n $d >> reports/pylint.log || :
     flake8 --max-complexity 8 $d >> reports/pyflakes.log || :
 done
-
