113a5c560dc0044ea1a704d9fe384df7cd38b898
Christopher A. Stelma <cstelma@tagged.com>
2014-03-07 16:19:27 -0800

    parse requirements.txt to give fpm something it can work with
    also, remove direct usage of requirements.txt from the build

--- .jenkins/jobs/suite.dsl.groovy
+++ .jenkins/jobs/suite.dsl.groovy
@@ -43,10 +43,6 @@ def matrixRPMs = project.pythonFPMMatrixJob {
     name 'build'
     logRotator(-1, 50)
 
-    environmentVariables([
-        'FPM_EXTRAS':'--python-obey-requirements-txt',
-    ])
-
     steps {
         publishers {
             archiveArtifacts('*.rpm')
