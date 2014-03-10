71d01c77dae4768220516c744a4947ff8f92146a
Mike Dougherty <mdougherty@tagged.com>
2014-03-10 15:58:15 -0700

    Created .pylintc (with pylint --generate-rcfile) and use it in pylint.sh

--- .jenkins/scripts/pylint.sh
+++ .jenkins/scripts/pylint.sh
@@ -4,6 +4,6 @@ scripts=$( dirname "${BASH_SOURCE-$0}" )
 source $scripts/python-setup.sh
 
 for d in tds tests ; do
-    pylint -f parseable -r n $d >> reports/pylint.log || :
+    pylint --rcfile=.pylintrc -f parseable -r n $d >> reports/pylint.log || :
     flake8 --max-complexity 8 $d >> reports/pyflakes.log || :
 done
