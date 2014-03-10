195890ac5fedde9a6a8ce9b0ac7be08f3bfec294
Mike Dougherty <mdougherty@tagged.com>
2014-03-10 16:08:47 -0700

    Some dosctrings

--- tests/factories/config.py
+++ tests/factories/config.py
@@ -1,3 +1,8 @@
+'''
+Factories for tds.util.config classes. They are backed by files in
+tests/fixtures/config.
+'''
+
 import tds.utils.config
 import factory
 
@@ -6,16 +11,19 @@ from .. import FIXTURES_PATH
 
 
 class ConfigFactory(factory.Factory):
+    'Superclass to redirect to tests/fixtures/config directory'
     FACTORY_FOR = tds.utils.config.TDSConfig
 
     conf_dir = join(FIXTURES_PATH, 'config')
 
 
 class DeployConfigFactory(ConfigFactory):
+    'A factory for a config that uses deploy.yml'
     FACTORY_FOR = tds.utils.config.TDSDeployConfig
 
 
 class TestDatabaseConfigFactory(ConfigFactory):
+    'A factory for a config that uses dbaccess.%(access_level)s.yml'
     FACTORY_FOR = tds.utils.config.TDSDatabaseConfig
 
     access_level = 'test'
