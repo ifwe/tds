fbf67599a55c8935d4d18b9f0308d76fa09ca21e
Mike Dougherty <mdougherty@tagged.com>
2014-03-12 11:07:21 -0700

    Add graphite notifications for some actions

--- tests/tds/notifications_test.py
+++ tests/tds/notifications_test.py
@@ -189,3 +189,35 @@ class TestHipchatNotifier(unittest2.TestCase):
                 },
                 'headers': {'Content-Length': '0'}
             }
+
+
+class TestGraphiteNotifier(unittest2.TestCase):
+    def setUp(self):
+        self.graphite_config = APP_CONFIG['notifications']['graphite']
+        self.notifier = tds.notifications.GraphiteNotifier(
+            APP_CONFIG,
+            self.graphite_config
+        )
+
+        self.graphite = patch('graphiteudp.GraphiteUDPClient').start()
+
+    def tearDown(self):
+        patch.stopall()
+
+    def test_inactive_event(self):
+        with patch.object(self.notifier, 'active_events', ()):
+            self.notifier.notify(DeploymentFactory())
+
+        assert not self.graphite.called
+
+    def test_active_event(self):
+        d = DeploymentFactory()
+        self.notifier.notify(d)
+
+        self.graphite.assert_called_with(**dict(
+            (x, self.graphite_config[x]) for x in ('host', 'port', 'prefix')
+        ))
+
+        self.graphite.return_value.send.assert_called_with(
+            d.package['name'], 1
+        )
