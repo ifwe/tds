8fdfd13874463b4631843cb75551fcb3866c8821
Mike Dougherty <mdougherty@tagged.com>
2014-03-13 14:28:41 -0700

    Split commands.py into commands/ package

---
+++ tds/commands/config.py
@@ -0,0 +1,70 @@
+import tagopsdb.database.meta
+import tagopsdb.deploy.repo
+import tagopsdb.exceptions
+
+import tds.utils
+import tds.exceptions
+from .repository import Repository
+from .deploy import Deploy
+
+
+class Config(Deploy):
+
+    """Commands to manage deployments for supported config applications"""
+
+    valid_project_types = ['tagconfig', 'kafka-config']
+    requires_tier_progression = False
+
+    @tds.utils.debug
+    def create(self, params):
+        # XXX: Replace this with a call to Repository(self.log).add(params)
+        """Add a new config project to the system"""
+
+        self.log.debug('Creating new config project')
+
+        tds.authorize.verify_access(params['user_level'], 'admin')
+
+        # Currently project type matches the project name
+        if params['project'] not in self.valid_project_types:
+            raise tds.exceptions.WrongProjectTypeError(
+                'Project "%s" is not valid for this command',
+                params['project']
+            )
+
+        try:
+            self.log.debug(5, 'Adding config project to repository')
+
+            # Project type matches project name
+            tagopsdb.deploy.repo.add_app_location(
+                params['project'],
+                params['buildtype'],
+                params['pkgname'],
+                params['project'],
+                params['pkgpath'],
+                params['arch'],
+                params['buildhost'],
+                params['env_specific']
+            )
+        except tagopsdb.exceptions.RepoException, e:
+            self.log.error(e)
+            return
+
+        tagopsdb.database.meta.Session.commit()
+        self.log.debug('Committed database changes')
+
+    @tds.utils.debug
+    def delete(self, params):
+        """Remove a config project from the system"""
+        return Repository(self.log).delete(params)
+
+    @tds.utils.debug
+    def push(self, params):
+        super(Config, self).promote(params)
+
+    @tds.utils.debug
+    def repush(self, params):
+        super(Config, self).redeploy(params)
+
+    @tds.utils.debug
+    def revert(self, params):
+        super(Config, self).rollback(params)
