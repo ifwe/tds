6b7c07e1842ce9a6a0c551850a8c0b4404957dfc
Mike Dougherty <mdougherty@tagged.com>
2014-03-17 13:12:45 -0700

    Split commands tests

--- tests/tds/commands/deploy_test.py
+++ tests/tds/commands/deploy_test.py
@@ -1,4 +1,4 @@
-from mock import Mock, patch
+from mock import patch
 from unittest_data_provider import data_provider
 import unittest2
 import logging
@@ -11,83 +11,20 @@ import tds.model
 import tds.utils.config as tds_config
 
 
-class _Base(unittest2.TestCase):
+class TestPromoteAndPush(unittest2.TestCase):
     def setUp(self):
         self.app_config = patch(
             'tds.utils.config.TDSDeployConfig',
             return_value=DeployConfigFactory(),
         )
         self.session = patch(
-            'tds.commands.Session',
+            'tagopsdb.database.meta.Session',
             **{'commit.return_value': None}
         ).start()
         self.tds_authorize = patch(
             'tds.authorize',
             **{'verify_access.return_value': True}
         ).start()
-
-    def tearDown(self):
-        patch.stopall()
-
-    def patch_method(self, obj, key, return_value):
-        patcher = patch.object(obj, key)
-        ptch = patcher.start()
-        ptch.return_value = return_value
-
-    force_provider = staticmethod(lambda: [
-        (True,),
-        (False,),
-    ])
-
-
-class TestPackageAdd(_Base):
-    def setUp(self):
-        super(TestPackageAdd, self).setUp()
-
-        self.package = tds.commands.Package(
-            logging.getLogger(type(self).__name__ + ' Package')
-        )
-
-        package_methods = [
-            ('_queue_rpm', True),
-        ]
-
-        for (key, return_value) in package_methods:
-            for obj in [self.package]:
-                self.patch_method(obj, key, return_value)
-
-    add_provider = lambda: [
-        (True, 'Not_None'),
-        (False, 'Not_None'),
-    ]
-
-    @data_provider(add_provider)
-    def test_add_package(self, force_option_used, pkg_state):
-        self.package_module = patch(
-            'tds.commands.package',
-            **{'add_package.return_value': None,
-               'find_package': Mock(status='completed')}
-        ).start()
-        self.repo_module = patch(
-            'tds.commands.repo',
-            **{'find_app_location': Mock(pkg_name='fake_app', arch='noarch')}
-        ).start()
-        self.patch_method(self.package, 'check_package_state', pkg_state)
-        self.patch_method(self.package, 'wait_for_state_change', None)
-
-        self.package.add(dict(
-            project='fake_app',
-            version='deadbeef',
-            user='fake_user',
-            user_level='fake_access',
-            repo={'incoming': 'fake_path'},
-            force=force_option_used
-        ))
-
-
-class TestPromoteAndPush(_Base):
-    def setUp(self):
-        super(TestPromoteAndPush, self).setUp()
 
         self.deploy = tds.commands.Deploy(
             logging.getLogger(type(self).__name__ + ' Deploy')
@@ -110,7 +47,15 @@ class TestPromoteAndPush(_Base):
             for obj in [self.deploy, self.config]:
                 self.patch_method(obj, key, return_value)
 
-    @data_provider(_Base.force_provider)
+    def tearDown(self):
+        patch.stopall()
+
+    def patch_method(self, obj, key, return_value):
+        patcher = patch.object(obj, key)
+        ptch = patcher.start()
+        ptch.return_value = return_value
+
+    @data_provider(lambda: [(True,), (False,)])
     def test_check_previous_environment(self, force_option_used):
         self.deploy.requires_tier_progression = True
         self.patch_method(
@@ -150,12 +95,7 @@ class TestPromoteAndPush(_Base):
 
         assert self.deploy.perform_deployments.called
 
-    promote_versions_provider = lambda: [
-        (True,),
-        (False,)
-    ]
-
-    @data_provider(promote_versions_provider)
+    @data_provider(lambda: [(True,), (False,)])
     def test_promote_version(self, version_is_new):
         self.patch_method(self.deploy, 'send_notifications', None)
         self.deploy.ensure_newer_versions.return_value = version_is_new
