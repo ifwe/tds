46355ccbc2556a9c2496a733c0776fa31a435ca2
Kenneth Lareau <klareau@tagged.com>
2014-03-20 17:57:17 -0700

    Reduce pylint warnings on tests/tds/model/actor_test.py
    pylint is now much happier with the test file.  Also add missing
    __init__.py files to avoid the "unable to import" warnings.

--- tests/tds/model/actor_test.py
+++ tests/tds/model/actor_test.py
@@ -1,10 +1,8 @@
 '''Tests for the actor model'''
 
-import os
-import pwd
 import unittest2
 
-from mock import Mock, mock_open, patch
+from mock import Mock, patch
 
 import tds.model
 
@@ -25,18 +23,19 @@ class TestActorModel(unittest2.TestCase):
 
     def test_name(self):
         'Ensure "name" property is functional'
-        getuid = patch(
+        patch(
             'os.getuid',
             return_value = 1234
         ).start()
-        getpwuid = patch(
+        patch(
             'pwd.getpwuid',
             return_value = [self.actor_properties['name']]
         ).start()
 
         assert self.actor.name == self.actor_properties['name']
 
-    def get_group_names(self, gid):
+    @staticmethod
+    def get_group_names(gid):
         'Method to return fake group name for fake gid'
         if gid == 501:
             return Mock(gr_name='fake_group1')
@@ -47,12 +46,12 @@ class TestActorModel(unittest2.TestCase):
 
     def test_groups(self):
         'Ensure "groups" property is functional'
-        getgroups = patch(
+        patch(
             'os.getgroups',
             return_value = [501, 502]
         ).start()
 
-        getgrgid = patch(
+        patch(
             'grp.getgrgid',
             side_effect = self.get_group_names
         ).start()
