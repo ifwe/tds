8bc75ef35b7d10c1aa0175098855f6280851f2ba
Christopher A. Stelma <cstelma@tagged.com>
2014-03-28 10:57:38 -0700

    docstrings

--- share/tds_cmd.py
+++ share/tds_cmd.py
@@ -2,6 +2,7 @@ import tds.scripts.tds_install as tds_install
 
 
 def _error_except(func):
+    """catch sys.exit System Errors"""
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
@@ -12,9 +13,11 @@ def _error_except(func):
 
 @_error_except
 def restart(self, app):
+    """restart via tds_install"""
     return tds_install.do_restart(app)
 
 
 @_error_except
 def install(self, app, version):
+    """install via tds_install"""
     return tds_install.do_install(app, version)
