7c3a18dffc50d2c734d14e45bf05c5b75b8d2e7e
Christopher A. Stelma <cstelma@tagged.com>
2014-03-27 19:30:31 -0700

    factor out caller/publish command

--- tds/deploy_strategy/tds_salt.py
+++ tds/deploy_strategy/tds_salt.py
@@ -11,16 +11,16 @@ logger = logging.getLogger('tds')
 
 class TDSSaltDeployStrategy(DeployStrategy):
 
+    def _publish(self, host, cmd, app):
+        caller = salt.client.Caller()
+        return caller.function('publish.publish', host, cmd, app)
+
     @tds.utils.debug
     def deploy_to_host(self, dep_host, app, version, retry=4):
-        caller = salt.client.Caller()
         logger.debug('Deploying to host %r', dep_host)
-        return caller.function('publish.publish', dep_host,
-                               'tds_install.install', app)
+        return self._publish(dep_host, 'tds_install.install', app)
 
     @tds.utils.debug
     def restart_host(self, dep_host, app, retry=4):
         """Restart application on a given host"""
-        caller = salt.client.Caller()
-        return caller.function('publish.publish', dep_host,
-                               'tds_install.restart', app)
+        return self._publish(dep_host, 'tds_install.restart', app)
