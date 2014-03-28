8981ee13b812575cbdc0e09083595d112f222538
Christopher A. Stelma <cstelma@tagged.com>
2014-03-27 20:24:07 -0700

    catch sys.exit

--- share/tds_cmd.py
+++ share/tds_cmd.py
@@ -1,9 +1,20 @@
 import tds.scripts.tds_install as tds_install
 
 
+def _error_except(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except SystemExit as e:
+            return 'error'
+    return wrapper
+
+
+@_error_except
 def restart(self, app):
     return tds_install.do_restart(app)
 
 
+@_error_except
 def install(self, app, version):
     return tds_install.do_install(app, version)
