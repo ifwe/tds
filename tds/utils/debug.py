9901798ad2357856b4924e4044bbd5bc9dd8a510
Mike Dougherty <mdougherty@tagged.com>
2014-04-01 14:43:02 -0700

    [lint] Some pylint for tds.utils.debug

--- tds/utils/debug.py
+++ tds/utils/debug.py
@@ -3,7 +3,7 @@ import logging
 __all__ = ['debug']
 
 
-def debug(f):
+def debug(func):
     """Decorator method to handle debug-level logging of code flow
     (method entry and exit notifications)
     """
@@ -16,29 +16,38 @@ def debug(f):
         if logger is None:
             logger = logging.getLogger('tds')
 
-        name = f.func_name
-        filename = f.func_code.co_filename
-        line_number = f.func_code.co_firstlineno
+        name = func.func_name
+        filename = func.func_code.co_filename
+        line_number = func.func_code.co_firstlineno
         typ = 'function'
 
         spacer = do_depth * getattr(debug, 'depth', 0) * ' '
 
-        logger.log(5, '%(spacer)sEntering %(typ)s %(name)s '
-                      '(%(filename)s:%(line_number)s). args=%(a)r, '
-                      'kwargs=%(k)r' % locals())
+        logger.log(
+            5,
+            '%(spacer)sEntering %(typ)s %(name)s '
+            '(%(filename)s:%(line_number)s). args=%(a)r, '
+            'kwargs=%(k)r', locals()
+        )
 
         try:
             setattr(debug, 'depth', getattr(debug, 'depth', 0) + 1)
-            return_val = f(*a, **k)
+            return_val = func(*a, **k)
         except BaseException, e:
-            logger.log(5, '%(spacer)sLeaving %(typ)s %(name)s '
-                          '(%(filename)s:%(line_number)s). '
-                          'exception=%(e)r', locals())
+            logger.log(
+                5,
+                '%(spacer)sLeaving %(typ)s %(name)s '
+                '(%(filename)s:%(line_number)s). '
+                'exception=%(e)r', locals()
+            )
             raise
         else:
-            logger.log(5, '%(spacer)sLeaving %(typ)s %(name)s '
-                          '(%(filename)s:%(line_number)s). '
-                          'returning=%(return_val)r' % locals())
+            logger.log(
+                5,
+                '%(spacer)sLeaving %(typ)s %(name)s '
+                '(%(filename)s:%(line_number)s). '
+                'returning=%(return_val)r', locals()
+            )
         finally:
             setattr(debug, 'depth', getattr(debug, 'depth', 1) - 1)
 
