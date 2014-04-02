c93d4898af132b0e051a8b436e524e291490f9b2
Mike Dougherty <mdougherty@tagged.com>
2014-04-01 18:14:10 -0700

    Disable similarities checker for pylint

--- .jenkins/scripts/pylint.sh
+++ .jenkins/scripts/pylint.sh
@@ -4,6 +4,6 @@ scripts=$( dirname "${BASH_SOURCE-$0}" )
 source $scripts/python-setup.sh
 
 for d in tds tests ; do
-    pylint --rcfile=.pylintrc -f parseable -r n $d >> reports/pylint.log || :
+    pylint --rcfile=.pylintrc -f parseable -r n --disable=similarities $d >> reports/pylint.log || :
     flake8 --max-complexity 8 $d >> reports/pyflakes.log || :
 done
