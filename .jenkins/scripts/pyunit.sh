51bbf0cc4c4fc139a35ada37fd12640708fb8945
Mike Dougherty <mdougherty@tagged.com>
2014-04-01 18:52:54 -0700

    [lint] shellcheck for pylint.sh and pyunit.sh
    (cherry picked from commit f2d6829d3cbf3bb1a68e1b92725f134b4a4af55c)

--- .jenkins/scripts/pyunit.sh
+++ .jenkins/scripts/pyunit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 scripts=$( dirname "${BASH_SOURCE-$0}" )
-source $scripts/python-setup.sh
+source "$scripts/python-setup.sh"
 
-$WORKSPACE/run_tests.py \
+"$WORKSPACE/run_tests.py" \
     -v \
     --junitxml=reports/pyunit.xml \
     --cov-report=xml \
     --cov=tds \
-    --ignore=$VIRTUAL_ENV ||:
+    --ignore="$VIRTUAL_ENV" ||:
