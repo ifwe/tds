a2f15b1990455494b10112d924bf2d7fe04fc9a2
Christopher A. Stelma <cstelma@tagged.com>
2014-04-29 17:31:24 -0700

    fixup update_deploy_repo rpm scripts

--- pkg/rpm/before_remove.sh
+++ pkg/rpm/before_remove.sh
@@ -1,5 +1,5 @@
 if [ $1 -eq 0 ] ; then
-    /sbin/service %{update_init} stop >/dev/null 2>&1
-    /sbin/chkconfig --del %{update_init}
+    /sbin/service <%= update_init %> stop >/dev/null 2>&1
+    /sbin/chkconfig --del <%= update_init %>
 fi
 
