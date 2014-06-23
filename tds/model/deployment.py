f5aaea19abaa4229a95dc0b07a4365c88c9d5f23
Mike Dougherty <mdougherty@tagged.com>
2014-06-23 16:50:51 -0700

    Reworking the unvalidated_deploy script

--- tds/model/deployment.py
+++ tds/model/deployment.py
@@ -1,7 +1,10 @@
 '''Model module for deployment object'''
 
 from .base import Base
-
+import tagopsdb
 
 class Deployment(Base):
     'Model class for deployment object'
+
+class AppDeployment(Deployment, tagopsdb.AppDeployment):
+    'Model class for deployment of an application'
