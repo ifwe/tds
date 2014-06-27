bb3dc0642a4e7abefc009dee36e485c5eb71ff7e
Mike Dougherty <mdougherty@tagged.com>
2014-06-26 17:39:12 -0700

    unvalidated_deploy_check: Add a test for notify

--- tests/tds/scripts/unvalidated_deploy_check_test.py
+++ tests/tds/scripts/unvalidated_deploy_check_test.py
@@ -9,10 +9,7 @@ from tests.factories.model.package import PackageFactory
 
 
 class TestUnvalidatedDeploymentNotifier(unittest2.TestCase):
-    def test_convert_deployment(self):
-        actor = ActorFactory()
-        package = PackageFactory()
-
+    def get_mock_app_dep(self, actor, package):
         tds_deployment = Mock(
             environment='test',
             package=package
@@ -21,11 +18,20 @@ class TestUnvalidatedDeploymentNotifier(unittest2.TestCase):
         application = Mock(
             app_type='fake_app_type'
         )
-        app_deployment = Mock(
+        return Mock(
             application=application,
             deployment=tds_deployment,
             user=actor.name,
         )
+
+    def test_convert_deployment(self):
+        actor = ActorFactory()
+        package = PackageFactory()
+
+        app_deployment = self.get_mock_app_dep(actor, package)
+        deployment = app_deployment.deployment
+        application = app_deployment.application
+
         result = UDC.UnvalidatedDeploymentNotifier.convert_deployment(app_deployment)
 
         # assert result.actor == actor  # XXX: skipped for now. groups don't match
@@ -33,5 +39,17 @@ class TestUnvalidatedDeploymentNotifier(unittest2.TestCase):
         assert result.action['command'] == 'unvalidated'
         assert result.project['name'] == package.name
         assert result.package == vars(package)  # TODO: shouldn't have to call vars
-        assert result.target['environment'] == tds_deployment.environment
+        assert result.target['environment'] == deployment.environment
         assert result.target['apptypes'] == [application.app_type]
+
+    def test_notify(self):
+        n = 2
+        actor = ActorFactory()
+        packages = [PackageFactory() for _ in range(n)]
+        deployments = [self.get_mock_app_dep(actor, p) for p in packages]
+
+        base = UDC.UnvalidatedDeploymentNotifier.__bases__[0]
+        with patch.object(base, 'notify'):
+            udn = UDC.UnvalidatedDeploymentNotifier(dict())
+            udn.notify(deployments)
+            assert base.notify.call_count == n
