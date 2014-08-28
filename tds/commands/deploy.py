f85ac4e603b96f52d85610d2e6aace5822d2b4cc
Mike Dougherty <mdougherty@tagged.com>
2014-08-28 13:25:34 -0700

    Return proper results from deploy commands (i.e. a dictionary)

--- tds/commands/deploy.py
+++ tds/commands/deploy.py
@@ -1889,12 +1889,11 @@ class DeployController(BaseController):
         app_dep_map = self.find_app_deployments(pkg_id, app_ids, params)
 
         if not len(filter(None, app_dep_map.itervalues())):
-            log.info(
+            raise Exception(
                 'Nothing to roll back for application %r in %s '
                 'environment', project.name,
                 self.envs[params['environment']]
             )
-            return
 
         # Save verison of application/deployment map for invalidation
         # at the end of the run
@@ -1914,6 +1913,8 @@ class DeployController(BaseController):
 
         tagopsdb.Session.commit()
         log.debug('Committed database changes')
+
+        return dict()
 
     @staticmethod
     def get_package_for_target(target, environment):
@@ -2055,12 +2056,11 @@ class DeployController(BaseController):
         app_dep_map = self.find_app_deployments(pkg_id, app_ids, params)
 
         if not len(filter(None, app_dep_map.itervalues())):
-            log.info(
+            raise Exception(
                 'Nothing to redeploy for application %r in %s '
                 'environment', project.name,
                 self.envs[params['environment']]
             )
-            return
 
         dep_id = self.determine_redeployments(pkg_id)
         self.send_notifications(project, params)
@@ -2068,6 +2068,8 @@ class DeployController(BaseController):
 
         tagopsdb.Session.commit()
         log.debug('Committed database changes')
+
+        return dict()
 
     @validate('project')
     def validate(self, project, **params):
@@ -2113,3 +2115,5 @@ class DeployController(BaseController):
 
         tagopsdb.Session.commit()
         log.debug('Committed database changes')
+
+        return dict()
