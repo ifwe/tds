49af81b229aeb2e5d1eb236f25bd07189f326902
Karandeep Singh Nagra <knagra@tagged.com>
2014-09-05 15:34:33 -0700

    moved JSON logic to TDSEncoder

--- tds/views/base.py
+++ tds/views/base.py
@@ -2,10 +2,6 @@
 Base class and helpers for tds.views
 """
 
-import json
-
-from .json_encoder import TDSEncoder
-
 
 class Base(object):
     """Base class and interface for a tds.views class."""
@@ -20,21 +16,3 @@ class Base(object):
         to the main application entry point.
         """
         raise NotImplementedError
-
-    def generate_json(self, iterable=None, obj=None):
-        """
-        Convert either iterable or obj to JSON.
-
-        obj and items in iterable should have to_dict() methods that return
-        a dictionary of fields and values.
-
-        If both iterable and obj are None, return the empty array "[]".
-        """
-        if iterable is not None:
-            return json.dumps([item.to_dict() for item in iterable
-                               if not isinstance(item, Exception)],
-                               cls=TDSEncoder)
-        elif obj is not None:
-            return json.dumps(obj.to_dict(), cls=TDSEncoder)
-        else:
-            return "[]"
