eefe1f6626e1e8b22ccee25bb2b223752fa19c60
Karandeep Singh Nagra <knagra@tagged.com>
2014-09-24 16:51:54 -0700

    lint; change names and isolate logic in logging_wrapper

--- tds/configure_logging.py
+++ tds/configure_logging.py
@@ -71,10 +71,10 @@ def configure_logging(config_dir, verbosity, use_color, daemon=False):
     logger = logging.getLogger()
     logger.setLevel(1)
     log_wrap.add_syslog(logger, 'syslog',
-                        facility=log_wrap.facilities.get(
+                        facility=log_wrap.FACILITIES.get(
                                 syslog_facility, log_wrap.LOG_LOCAL4
                         ),
-                        priority=log_wrap.priorities.get(
+                        priority=log_wrap.PRIORITIES.get(
                                 syslog_priority, log_wrap.LOG_DEBUG
                         ))
 
