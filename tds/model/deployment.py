044f08ae48266ea8d54484353194546189ceccc0
Kenneth Lareau <klareau@tagged.com>
2014-11-18 15:07:59 -0800

    Add a model for HostDeployment and set some properties

--- tds/model/deployment.py
+++ tds/model/deployment.py
@@ -7,8 +7,26 @@ import tagopsdb
 class Deployment(Base):
     """Model class for deployment object."""
 
+    @property
+    def application(self):
+        return self.delegate.deployment.package.application
+
 
 class AppDeployment(Deployment):
-    """Model class for deployment of an application."""
+    """Model class for deployment of an application to a tier."""
 
     delegate = tagopsdb.AppDeployment
+
+
+class HostDeployment(Deployment):
+    """Model class for deployment of an application to a host."""
+
+    delegate = tagopsdb.HostDeployment
+
+    @property
+    def app_target(self):
+        return self.delegate.host.target
+
+    @property
+    def host_state(self):
+        return self.delegate.host.state
