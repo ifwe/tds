572c4eb6ad9814032586b08727a547800a91347b
Mike Dougherty <mike@ifwe.co>
2014-12-09 17:27:53 -0800

    Make salt subprocess call more better

--- tds/deploy_strategy/tds_salt.py
+++ tds/deploy_strategy/tds_salt.py
@@ -1,6 +1,6 @@
 """Salt-based DeployStrategy."""
 
-import salt.client
+import json
 import tds.utils
 
 from .base import DeployStrategy
@@ -12,11 +12,40 @@ log = logging.getLogger('tds')
 class TDSSaltDeployStrategy(DeployStrategy):
     """Salt (master publish.publish) based DeployStrategy."""
 
-    @staticmethod
-    def _publish(host, cmd, app):
+    def __init__(self, bin=None, user=None, c_dir=None, **kwargs):
+        self.bin = bin or 'salt-call'
+        self.user = user
+        self.c_dir = c_dir
+
+    def _publish(self, host, action, app):
         """Dispatch to salt master."""
-        caller = salt.client.Caller()
-        return caller.function('publish.publish', host, cmd, app)
+
+        user_cmd = []
+
+        if self.user is not None:
+            user_cmd = ['sudo', '-u', self.user]
+
+        salt_opts = ['--out', 'json']
+        if self.c_dir is not None:
+            salt_opts += ['-c', self.c_dir]
+
+        salt_cmd = [self.bin, 'salt-call', 'publish.publish']
+        publish_args = [host, action, app]
+        cmd = user_cmd + salt_cmd + salt_opts + publish_args
+
+        proc = tds.utils.processes.run(cmd, expect_return_code=None)
+
+        info = {}
+        if proc.stdout:
+            try:
+                info = json.loads(proc.stdout)
+            except ValueError:
+                log.error(
+                    "Could not decode result from salt. stdout=%r, stderr=%r",
+                    proc.stdout, proc.stderr
+                )
+
+        return proc.returncode == 0, info
 
     @tds.utils.debug
     def deploy_to_host(self, dep_host, app, version, retry=4):
