75deec8a14bf9d4f03aeb8510d0f877b2fdb05cd
Karandeep Singh Nagra <knagra@tagged.com>
2015-01-09 13:37:04 -0800

    Add docstrings to tds.utils.rpm

--- tds/utils/rpm.py
+++ tds/utils/rpm.py
@@ -1,4 +1,4 @@
-""""""
+"""Classes for working with RPMs."""
 
 import logging
 import os.path
@@ -10,8 +10,15 @@ log = logging.getLogger('tds.utils.rpm')
 
 
 class RPMQueryProvider(object):
+    """
+    Provider for querying of data on an RPM file.
+    """
+
     @classmethod
     def query(cls, filename, fields):
+        """
+        Query for given fields from file filename, assuming it's a valid RPM.
+        """
         rpm_format = '\n'.join(
             ('%%{%s}' % field) for field in fields
         )
@@ -34,6 +41,10 @@ class RPMQueryProvider(object):
 
 
 class RPMDescriptor(object):
+    """
+    Descriptor for an RPM.
+    """
+
     query_fields = ('arch', 'name', 'version', 'release')
     arch = None
     name = None
@@ -41,6 +52,10 @@ class RPMDescriptor(object):
     release = None
 
     def __init__(self, path, **attrs):
+        """
+        Set up this descriptor for the given file path and attributes for the
+        RPM.
+        """
         self.path = path
 
         for key, val in attrs.items():
@@ -48,10 +63,18 @@ class RPMDescriptor(object):
 
     @property
     def filename(self):
+        """
+        Return the filename of the RPM being described.
+        If the full path to the file is "/opt/rpms/myrpm.rpm",
+        then this will return "myrpm.rpm"
+        """
         return os.path.basename(self.path)
 
     @property
     def package_info(self):
+        """
+        Return a select subset of the attributes of the RPM being described.
+        """
         info = self.__dict__.copy()
         info.pop('path', None)
         info['revision'] = info.pop('release', None)
@@ -60,6 +83,9 @@ class RPMDescriptor(object):
 
     @property
     def info(self):
+        """
+        Return info on the RPM being described.
+        """
         #TODO We shouldn't be doing this dict pop trick.
         # Mike recommended possibly adding a hybrid property to the
         # tagopsdb.model.package.Package class.
@@ -69,6 +95,9 @@ class RPMDescriptor(object):
 
     @classmethod
     def from_path(cls, path):
+        """
+        Build a new RPM descriptor from the given path.
+        """
         info = RPMQueryProvider.query(path, RPMDescriptor.query_fields)
         if info is None:
             return None
