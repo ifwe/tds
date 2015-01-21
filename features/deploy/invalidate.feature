ifwe/tds

commit cf3c9d630bb0fb5f55ffdd74116670e3e3743074

Check that hosts are associated with a tier [TDS-28]

features/deploy/invalidate.feature
@@ -37,8 +37,20 @@ Feature: deploy invalidate application version [--apptypes|--all-apptypes]
         When I run "deploy invalidate myapp 123 --apptype foo bar"
         Then the output is "Valid apptypes for application "myapp" are: ['foo']"
 
+    Scenario: invalidate a package from an application with no hosts associated
+        Given there is a deploy target with name="foo"
+        And the deploy target is a part of the project-application pair
+        And the package is deployed on the deploy target
+        When I run "deploy invalidate myapp 123 --apptype foo"
+        Then the output is "No hosts are associated with the app tier 'foo' in the dev environment"
+
     Scenario: invalidate a package from an application for an apptype with it currently deployed
         Given there is a deploy target with name="foo"
+        And there are hosts:
+            | name      |
+            | dhost01   |
+            | dhost02   |
+        And the hosts are associated with the deploy target
         And the deploy target is a part of the project-application pair
         And the package is deployed on the deploy target
         When I run "deploy invalidate myapp 123 --apptype foo"
@@ -46,6 +58,11 @@ Feature: deploy invalidate application version [--apptypes|--all-apptypes]
 
     Scenario: invalidate a package from an application for an apptype
         Given there is a deploy target with name="foo"
+        And there are hosts:
+            | name      |
+            | dhost01   |
+            | dhost02   |
+        And the hosts are associated with the deploy target
         And the deploy target is a part of the project-application pair
         And the package is deployed on the deploy target
         When I run "deploy invalidate myapp 123 --apptype foo"
@@ -53,6 +70,11 @@ Feature: deploy invalidate application version [--apptypes|--all-apptypes]
 
     Scenario: invalidate a package from an application with single apptype
         Given there is a deploy target with name="foo"
+        And there are hosts:
+            | name      |
+            | dhost01   |
+            | dhost02   |
+        And the hosts are associated with the deploy target
         And the deploy target is a part of the project-application pair
         And the package is deployed on the deploy target
         And the package has been validated
@@ -62,6 +84,11 @@ Feature: deploy invalidate application version [--apptypes|--all-apptypes]
 
     Scenario: invalidate a package from an application with single apptype with apptype option
         Given there is a deploy target with name="foo"
+        And there are hosts:
+            | name      |
+            | dhost01   |
+            | dhost02   |
+        And the hosts are associated with the deploy target
         And the deploy target is a part of the project-application pair
         And the package is deployed on the deploy target
         And the package has been validated
@@ -82,8 +109,13 @@ Feature: deploy invalidate application version [--apptypes|--all-apptypes]
     Scenario: invalidate a package from an application with multiple apptypes with apptype option
         Given there are deploy targets:
             | name  |
-            | foo   |
             | bar   |
+            | foo   |
+        And there are hosts:
+            | name      |
+            | dhost01   |
+            | dhost02   |
+        And the hosts are assoicated with the deploy target
         And the deploy targets are a part of the project-application pair
         And the package is deployed on the deploy targets
         And the package has been validated
@@ -91,10 +123,17 @@ Feature: deploy invalidate application version [--apptypes|--all-apptypes]
         Then the package is invalidated for deploy target with name="foo"
 
     Scenario: invalidate a package from an application with multiple apptypes with all-apptypes option
-        Given there are deploy targets:
-            | name  |
-            | foo   |
-            | bar   |
+        Given there is a deploy target with name="foo"
+        And there are hosts:
+            | name      |
+            | dfoo01    |
+            | dfoo02    |
+        And the hosts are associated with the deploy target
+        And there is a deploy target with name="bar"
+        And there is a host with name="dbar01"
+        And the host is associated with the deploy target
+        And there is a host with name="dbar02"
+        And the host is associated with the deploy target
         And the deploy targets are a part of the project-application pair
         And the package is deployed on the deploy targets
         And the package has been validated

features/deploy/promote.feature
@@ -33,6 +33,14 @@ Feature: deploy promote application version [-f|--force] [--delay] [--hosts|--ap
         And the deploy target is a part of the project-application pair
         And the hosts are associated with the deploy target
 
+    Scenario: promote application to a tier with no hosts
+        Given there is a deploy target with name="other-apptype"
+        And the deploy target is a part of the project-application pair
+        And the package is deployed on the deploy targets in the "dev" env
+        And the package has been validated in the "development" environment
+        When I run "deploy promote myapp 123 --apptype other-apptype"
+        Then the output is "No hosts are associated with the app tier 'other-apptype' in the stage environment"
+
     Scenario: promote application that doesn't exist
         When I run "deploy promote badapp 456"
         Then the output is "Application does not exist: badapp"

features/deploy/validate.feature
@@ -77,24 +77,22 @@ Feature: deploy validate application version [-f|--force] [--apptypes|--all-appt
 
     Scenario: validate a package from an application with multiple apptypes with apptype option
         Given there is a deploy target with name="bar"
-        And there are hosts:
-            | name           | env  |
-            | anotherhost01  | dev  |
-            | anotherhost02  | dev  |
+        And there is a host with name="anotherhost01",env="dev"
+        And the host is associated with the deploy target
+        And there is a host with name="anotherhost02",env="dev"
+        And the host is associated with the deploy target
         And the deploy target is a part of the project-application pair
-        And the hosts are associated with the deploy target
         And the package is deployed on the deploy target
         When I run "deploy validate myapp 123 --apptype foo"
         Then the package is validated for deploy target with name="foo"
 
     Scenario: validate a package from an application with multiple apptypes with all-apptypes option
         Given there is a deploy target with name="bar"
-        And there are hosts:
-            | name           | env  |
-            | anotherhost01  | dev  |
-            | anotherhost02  | dev  |
+        And there is a host with name="anotherhost01",env="dev"
+        And the host is associated with the deploy target
+        And there is a host with name="anotherhost02",env="dev"
+        And the host is associated with the deploy target
         And the deploy target is a part of the project-application pair
-        And the hosts are associated with the deploy target
         And the package is deployed on the deploy target
         When I run "deploy validate myapp 123 --all-apptypes"
         Then the package is validated for the deploy targets

tds/commands/deploy.py
@@ -912,6 +912,15 @@ class DeployController(BaseController):
         else:
             log.log(5, 'Verification is for application tiers...')
 
+            for apptype in apptypes:
+                if not tagopsdb.Host.find(application=apptype):
+                    raise tds.exceptions.InvalidOperationError(
+                        ("No hosts are associated with the app tier '%s' in "
+                         "the %s environment"),
+                        apptype.name,
+                        params['env'],
+                    )
+
         log.log(5, 'Package ID is: %s', package.id)
         log.log(
             5, 'Application IDs are: %s',

commit 5823fc3c4ac10511bdefa1e4194b4847c195d9ad

Fix features broken by cf3c9d6

features/deploy/invalidate.feature
@@ -115,7 +115,7 @@ Feature: deploy invalidate application version [--apptypes|--all-apptypes]
             | name      |
             | dhost01   |
             | dhost02   |
-        And the hosts are assoicated with the deploy target
+        And the hosts are associated with the deploy target
         And the deploy targets are a part of the project-application pair
         And the package is deployed on the deploy targets
         And the package has been validated

features/notification/email.feature
@@ -19,12 +19,11 @@ Feature: email notifications
         And the deploy target is a part of the project-application pair
         And the hosts are associated with the deploy target
         And there is a deploy target with name="another-apptype"
-        And there are hosts:
-            | name            | env   |
-            | danotherhost01  | dev   |
-            | danotherhost02  | dev   |
+        And there is a host with name="danotherhost01",env="dev"
+        And the host is associated with the deploy target
+        And there is a host with name="danotherhost02",env="dev"
+        And the host is associated with the deploy target
         And the deploy target is a part of the project-application pair
-        And the hosts are associated with the deploy target
 
     @email_server
     Scenario: deployment of single apptype occurs with working mail notification