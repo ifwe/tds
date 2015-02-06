592e32713a430e8c8b1f7cfc3510aaa7634bc96c
Karandeep Singh Nagra <knagra@tagged.com>
2015-02-06 15:19:59 -0800

    Add application REST code and features

--- tds/views/rest/applications.py
+++ tds/views/rest/applications.py
@@ -1,5 +1,5 @@
 """
-REST API view for projects.
+REST API view for applications.
 """
 
 from cornice.resource import resource, view
@@ -9,14 +9,14 @@ from . import utils
 import tds.model
 
 
-@resource(collection_path="/projects", path="/projects/{name_or_id}")
-class ProjectView(object):
+@resource(collection_path="/applications", path="/applications/{name_or_id}")
+class ApplicationView(object):
     """
-    Project view. This object maps to the /projects and /projects/{name_or_id}
-    URL.
+    Application view. This object maps to the /applications and
+    /applications/{name_or_id} URL.
     An object of this class is initalized to handle each request.
-    The collection_* methods correspond to the /projects URL while the others
-    correspond to the /projects/{name_or_id} URL.
+    The collection_* methods correspond to the /applications URL while the
+    others correspond to the /applications/{name_or_id} URL.
     """
 
     def __init__(self, request):
@@ -29,19 +29,19 @@ class ProjectView(object):
 
     def validate_individual(self, request):
         """
-        Validate that the project with given name exists and attach
-        the project to the request at request.validated['project'].
+        Validate that an application with the given name_or_id in the request
+        URL exists and attach the application to the request at
+        request.validated['application'].
         This validator can raise a "400 Bad Request" error.
         """
-        utils.get_obj_by_name_or_id('project', request)
+        utils.get_obj_by_name_or_id('application', request)
 
     def validate_collection_get(self, request):
         """
-        Make sure that the selection parameters are valid for projects.
-        If they are not, raise "400 Bad Request".
-        Else, set request.validated['projects'] to projects matching query.
+        Get collection of applications matching given query parameters.
+        Can raise "400 Bad Request" if unsupported params in query.
         """
-        utils.get_collection_by_limit_start('project', request)
+        utils.get_collection_by_limit_start('application', request)
 
     @view(validators=('validate_individual',))
     def delete(self):
@@ -49,19 +49,19 @@ class ProjectView(object):
         Delete an individual project.
         """
         #TODO Implement the delete part.
-        return utils.make_response(request.validated['project'])
+        return utils.make_response(request.validated['application'])
 
     @view(validators=('validate_individual',))
     def get(self):
         """
-        Return an individual project.
+        Return an individual application.
         """
-        return utils.make_response(self.request.validated['project'])
+        return utils.make_response(self.request.validated['application'])
 
     @view(validators=('validate_individual',))
     def put(self):
         """
-        Update an existing project.
+        Update an existing application.
         """
         #TODO Implement this
         return utils.make_response({})
@@ -69,18 +69,18 @@ class ProjectView(object):
     @view(validators=('validate_collection_get',))
     def collection_get(self):
         """
-        Return a list of matching projects for the query in the request.
+        Return a list of matching applications for the query in the request.
         Request parameters:
-            Request should either be empty (match all projects) or contain
+            Request should either be empty (match all applications) or contain
             a 'limit' and 'start' parameters for pagination.
         Returns:
             "200 OK" if valid request successfully processed
         """
-        return utils.make_response(self.request.validated['projects'])
+        return utils.make_response(self.request.validated['applications'])
 
     def collection_post(self):
         """
-        Create a new project with given params.
+        Create a new application
         """
         #TODO Implement this
         return utils.make_response([])
--- tds/views/rest/utils.py
+++ tds/views/rest/utils.py
@@ -6,6 +6,8 @@ import json
 
 from pyramid.response import Response
 
+import tds.model
+
 from ..json_encoder import TDSEncoder
 
 
@@ -23,3 +25,116 @@ def make_response(body, status="200 OK", renderer="json"):
         raise NotImplementedError("REST renderer not implemented: {r}".format(
             r=renderer,
         ))
+
+
+def get_obj_by_name_or_id(obj_type, request):
+    """
+    Validate that an object of type obj_type with the name_or_id given in the
+    request exists and attach it to the request at
+    request.validated[obj_type].
+    Attach an error with location='path', name='name_or_id' and a description.
+    This error will return a "400 Bad Request" response to this request.
+    """
+    obj_cls = getattr(tds.model, obj_type.title(), None)
+    if obj_cls is None:
+        raise tds.exceptions.NotFoundError('Model', [obj_type])
+    try:
+        obj_id = int(request.matchdict['name_or_id'])
+        obj = obj_cls.get(id=obj_id)
+        name = False
+    except ValueError:
+        obj_id = False
+        name = request.matchdict['name_or_id']
+        obj = obj_cls.get(name=name)
+    if obj is None:
+        request.errors.add(
+            'path', 'name_or_id',
+            "{obj_type} with {prop} {val} does not exist".format(
+                obj_type=obj_type.title(),
+                prop="ID" if obj_id else "name",
+                val=obj_id if obj_id else name,
+            )
+        )
+        request.errors.status = 404
+    else:
+        request.validated[obj_type] = obj
+
+
+def get_collection_by_limit_start(obj_type, request):
+    """
+    Make sure that the selection parameters are valid for collection GET.
+    If they are not, raise "400 Bad Request".
+    Else, set request.validated[obj_type + 's'] to objects matching query.
+    """
+    obj_cls = getattr(tds.model, obj_type.title(), None)
+    if obj_cls is None:
+        raise tds.exceptions.NotFoundError('Model', [obj_type])
+    plural = obj_type + 's'
+    all_params = ('limit', 'start',) # for later: 'sort_by', 'reverse')
+    for key in request.params:
+        if key not in all_params:
+            request.errors.add(
+                'query', key,
+                ("Unsupported query: {param}. Valid parameters: "
+                 "{all_params}".format(param=key, all_params=all_params))
+            )
+
+    # This might be used later but isn't currently:
+    # if 'sort_by' in request.params:
+    #     valid_attrs = ('id', 'name')
+    #     if request.params['sort_by'] not in valid_attrs:
+    #         request.errors.add(
+    #             'query', 'sort_by',
+    #             ("Unsupported sort attribute: {val}. Valid attributes: "
+    #              "{all_attrs}".format(
+    #                 val=request.params['sort_by'],
+    #                 all_attrs=valid_attrs,
+    #              ))
+    #         )
+    #     elif request.params['sort_by'] == 'name':
+    #         sort_by = tds.model.Project.name
+    #     else:
+    #         sort_by = tds.model.Project.id
+    # request.validated['projects'].order_by(sort_by)
+
+    if 'start' in request.params and request.params['start']:
+        request.validated[plural] = (
+            obj_cls.query().order_by(obj_cls.id).filter(
+                obj_cls.id >= request.params['start']
+            )
+        )
+    else:
+        request.validated[plural] = (
+            obj_cls.query().order_by(obj_cls.id)
+        )
+
+    if obj_cls == tds.model.Application:
+        request.validated[plural] = (
+            request.validated[plural].filter(obj_cls.pkg_name != '__dummy__')
+        )
+
+    if 'limit' in request.params and request.params['limit']:
+        request.validated[plural] = (
+            request.validated[plural].limit(request.params['limit'])
+        )
+
+    # This code was designed to allow filtering on attributes.
+    # It's preserved here for ideas in case it's relevant elsewhere.
+    # names = set(request.params.getall('name'))
+    # proj_ids = set(request.params.getall('id'))
+    #
+    # if proj_ids and names:
+    #     request.validated['projects'] = tds.model.Project.query().filter(
+    #         tds.model.Project.id.in_(tuple(proj_ids)),
+    #         tds.model.Project.name.in_(tuple(names)),
+    #     )
+    # elif proj_ids:
+    #     request.validated['projects'] = tds.model.Project.query().filter(
+    #         tds.model.Project.id.in_(tuple(proj_ids)),
+    #     )
+    # elif names:
+    #     request.validated['projects'] = tds.model.Project.query().filter(
+    #         tds.model.Project.name.in_(tuple(names)),
+    #     )
+    # else:
+    #     request.validated['projects'] = tds.model.Project.all()
