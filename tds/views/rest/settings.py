e0836bd94e4f787f044e3f4097adf79245ae1860
Karandeep Singh Nagra <knagra@tagged.com>
2015-05-26 14:16:53 -0700

    Fix up bugs in cookie code

--- tds/views/rest/settings.py
+++ tds/views/rest/settings.py
@@ -5,5 +5,5 @@ REST API settings.
 # Secret key for encrypting cookie digests.
 SECRET_KEY = b'super secret'
 
-# Duration of cookies in seconds.
+# Valid life of cookies in seconds.
 COOKIE_LIFE = 15 * 24 * 60 * 60
