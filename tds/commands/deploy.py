9382f104d36a9fe16c1eacc0b782ced451eb3879
Kenneth Lareau <klareau@tagged.com>
2015-08-19 14:28:49 -0700

    Initial ideas for deploy subcommand flow in comments

--- tds/commands/deploy.py
+++ tds/commands/deploy.py
@@ -2,9 +2,12 @@
 Controller for 'deploy' commands.
 """
 
+import logging
+
 from .base import BaseController, validate as input_validate
 
 log = logging.getLogger('tds')
+
 
 class DeployController(BaseController):
     """
@@ -40,13 +43,22 @@ class DeployController(BaseController):
     @input_validate('application')
     def fix(self, application, package, hosts=None, apptypes=None,
             **params):
+        # For the given hosts or tiers, determine if there are any failed
+        # host deployments.  If so, re-attempt the deployments after ensuring
+        # no other conflicting deployments are in progress; if they
+        # succeed and it's a tier deployment, update the tier as 'complete'
+        # as well, otherwise leave as 'incomplete'.
         pass
 
     @input_validate('package')
     @input_validate('targets')
     @input_validate('application')
     def invalidate(self, application, package, hosts=None, apptypes=None,
-                  **params):
+                   **params):
+        # Ensure that, for the tiers we're checking, the given package
+        # had been deployed at some point and is in the 'validated' state.
+        # If so, update tier to 'invalidated', otherwise do nothing
+        # (and mention nothing has been done).
         pass
 
     @input_validate('package')
@@ -54,6 +66,10 @@ class DeployController(BaseController):
     @input_validate('application')
     def promote(self, application, package, hosts=None, apptypes=None,
                 **params):
+        # For the given hosts for tiers, ensure the given package is not
+        # currently deployed (do nothing if this is the case), then attempt
+        # the deployments.... okay, this one is really complicated, let's
+        # discuss this one more.
         pass
 
     @input_validate('package')
@@ -61,6 +77,9 @@ class DeployController(BaseController):
     @input_validate('application')
     def restart(self, application, package, hosts=None, apptypes=None,
                 **params):
+        # For the given hosts or tiers, restart the appropriate service
+        # on each one for the given package and inform if restart succeeded
+        # or failed.
         pass
 
     @input_validate('package')
@@ -68,12 +87,20 @@ class DeployController(BaseController):
     @input_validate('application')
     def rollback(self, application, package, hosts=None, apptypes=None,
                  **params):
+        # For the given hosts or tiers, ensure the package is installed.
+        # If so, determine the previous deployed and validated version;
+        # if there's one, install that version and for a tier rollback
+        # set the status to 'invalidated' (nothing for hosts), else throw
+        # appropriate error.  If no previous validated version, throw
+        # appropriate error as well.
         pass
 
     @input_validate('package_show')
     @input_validate('targets')
     @input_validate('application')
     def show(self, application, package, apptypes=None, **params):
+        # For the given application/package and given apptypes,
+        # return the current deployment information (if any).
         pass
 
     @input_validate('package')
