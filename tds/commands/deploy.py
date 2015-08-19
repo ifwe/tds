2cd2525fd320358744f14b7801ba5684de323af0
Karandeep Nagra <knagra@ifwe.co>
2015-08-18 17:18:14 -0700

    Add skeleton for new DeployController

--- tds/commands/deploy.py
+++ tds/commands/deploy.py
@@ -1,4 +1,84 @@
 """
 Controller for 'deploy' commands.
 """
-# PUT LOTS OF NEW STUFF HERE
+
+from .base import BaseController, validate as input_validate
+
+log = logging.getLogger('tds')
+
+class DeployController(BaseController):
+    """
+    Commands to manage deployments.
+    """
+
+    dep_types = {'promote': 'Deployment',
+                 'fix': 'Fix',
+                 'rollback': 'Rollback',
+                 'push': 'Push',
+                 'repush': 'Repush',
+                 'revert': 'Reversion', }
+    envs = {'dev': 'development',
+            'stage': 'staging',
+            'prod': 'production', }
+    env_order = ['dev', 'stage', 'prod']
+
+    requires_tier_progression = True
+
+    access_levels = {
+        'invalidate': 'environment',
+        'show': 'environment',
+        'validate': 'environment',
+        'promote': 'environment',
+        'fix': 'environment',
+        'redeploy': 'environment',
+        'rollback': 'environment',
+        'restart': 'environment',
+    }
+
+    @input_validate('package_hostonly')
+    @input_validate('targets')
+    @input_validate('application')
+    def fix(self, application, package, hosts=None, apptypes=None,
+            **params):
+        pass
+
+    @input_validate('package')
+    @input_validate('targets')
+    @input_validate('application')
+    def invalidate(self, application, package, hosts=None, apptypes=None,
+                  **params):
+        pass
+
+    @input_validate('package')
+    @input_validate('targets')
+    @input_validate('application')
+    def promote(self, application, package, hosts=None, apptypes=None,
+                **params):
+        pass
+
+    @input_validate('package')
+    @input_validate('targets')
+    @input_validate('application')
+    def restart(self, application, package, hosts=None, apptypes=None,
+                **params):
+        pass
+
+    @input_validate('package')
+    @input_validate('targets')
+    @input_validate('application')
+    def rollback(self, application, package, hosts=None, apptypes=None,
+                 **params):
+        pass
+
+    @input_validate('package_show')
+    @input_validate('targets')
+    @input_validate('application')
+    def show(self, application, package, apptypes=None, **params):
+        pass
+
+    @input_validate('package')
+    @input_validate('targets')
+    @input_validate('application')
+    def validate(self, application, package, hosts=None, apptypes=None,
+                 **params):
+        pass
