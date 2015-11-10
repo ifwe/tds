f20a33f70ce796b7844ec0427177f08bf950f000
Karandeep Nagra <knagra@ifwe.co>
2015-11-10 13:58:53 -0800

    Some work on deploy fix

--- tds/model/application.py
+++ tds/model/application.py
@@ -77,6 +77,15 @@ class Application(Base):
         defaults.update(attrs)
         return Package.create(**defaults)
 
+    def get_latest_tier_deployment(self, tier_id, environment_id):
+        return tagopsdb.Session.query(tagopsdb.model.AppDeployment).join(
+            tagopsdb.model.AppDeployment.package
+        ).filter(
+            tagopsdb.model.Package.pkd_def_id == self.id,
+            tagopsdb.model.AppDeployment.app_id == tier_id,
+            tagopsdb.model.AppDeployment.environment_id == environment_id,
+        ).order_by(desc(tagopsdb.model.AppDeployment.realized)).first()
+
     def get_latest_completed_tier_deployment(self, tier_id, environment_id,
                                              must_be_validated=False):
         in_list = ['validated'] if must_be_validated else ['validated',
@@ -89,6 +98,14 @@ class Application(Base):
             tagopsdb.model.AppDeployment.environment_id == environment_id,
             tagopsdb.model.AppDeployment.status.in_(in_list)
         ).order_by(desc(tagopsdb.model.AppDeployment.realized)).first()
+
+    def get_latest_host_deployment(self, host_id):
+        return tagopsdb.Session.query(tagopsdb.model.HostDeployment).join(
+            tagopsdb.model.HostDeployment.package
+        ).filter(
+            tagopsdb.model.Package.pkg_def_id == self.id,
+            tagopsdb.model.HostDeployment.host_id == host_id,
+        ).order_by(desc(tagopsdb.model.HostDeployment.realized)).first()
 
     def get_latest_completed_host_deployment(self, host_id):
         return tagopsdb.Session.query(tagopsdb.model.HostDeployment).join(
