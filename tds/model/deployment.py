39ad718477092939ee889b11315ba4ba0d812619
Kenneth Lareau <klareau@tagged.com>
2016-01-19 13:54:11 -0800

    Undo Mike damage.

--- tds/model/deployment.py
+++ tds/model/deployment.py
@@ -10,10 +10,7 @@ from .deploy_target import AppTarget
 class Deployment(Base):
     """Model class for deployment object."""
 
-    # TO FIX: Can't use a delegate here right now due to
-    # the 'convert_deployment' method in tds/utils/script_helpers.py
-    # Once that's fixed, this can be enabled
-    #delegate = tagopsdb.Deployment
+    delegate = tagopsdb.Deployment
 
 
 class AppDeployment(Deployment):
