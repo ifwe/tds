d1582dad604ab27bb9b43f522ce389773508704a
Kenneth Lareau <klareau@tagged.com>
2016-03-08 12:52:36 -0800

    Update branch and version for tagopsdb requirement

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ def load_requirements(fname):
 
 REQUIREMENTS = {}
 REQUIREMENTS['install'] = load_requirements('requirements.txt')
-REQUIREMENTS['install'].append('tagopsdb>=0.9.17')
+REQUIREMENTS['install'].append('tagopsdb>=0.9.18')
 REQUIREMENTS['tests'] = load_requirements('requirements-dev.txt')
 
 def load_github_dependency_links(fname):
