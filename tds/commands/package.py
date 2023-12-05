c8f4aec97b1b2e96cd5918aed4810ccdab6f0404
Stephen Paul Lesniewski <slesniewski@meetme.com>
2023-12-05 14:47:11 -0500

    SRE-17954: update TDS wiki link to to use current Confluence server

--- tds/commands/package.py
+++ tds/commands/package.py
@@ -182,7 +182,7 @@ class PackageController(BaseController):
             log.info(
                 "WARNING: Fingerprinting is not enabled, your RPM "
                 "will not be validated first.  Please see:\n"
-                "https://wiki.ifwe.co/display/siteops/Enabling+"
+                "https://confluence.meetmecorp.com/display/SO/Enabling+"
                 "fingerprinting+for+archived+files+in+Jenkins\n"
                 "for information on how to enable it. Continuing..."
             )
